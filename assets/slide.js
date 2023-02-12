1f092212a0e537ba48ea431380e32d19f2de3967
loveshopify25 <loveshopify25@gmail.com>
2023-02-12 19:09:24 +0200

    complete slie

--- assets/slide.js
+++ assets/slide.js
@@ -1,22 +1,16 @@
 
 
-let imgInterval = setInterval(() => {
-  if(document.querySelector('.mobile-show.image-slider')){
-    clearTimeout(imgInterval);
-    $('.mobile-show.image-slider').each(function(){
-      $(this).slick({
-          // autoplay:true,
-          arrows: true,
-          draggable: true,
-          drag: true,
-          dots: true,    //       infinite: true,
-          speed: 200,
-          slidesToShow: 1,
-      });
-    })
-  }
-	
-}, 100);
+$('.mobile-show.image-slider').each(function(){
+  $(this).slick({
+      // autoplay:true,
+      arrows: true,
+      draggable: true,
+      drag: true,
+      dots: true,    //       infinite: true,
+      speed: 200,
+      slidesToShow: 1,
+  });
+})
 
 $(document).on('click','.size-popup-button',function(){
   $('#shopify-section-header-inline').addClass('layout-lower');
@@ -34,6 +28,9 @@ $(document).on('click','.mobile-show.desc-heading',function(e){
   $(e.target).toggleClass('is_open');
   $(e.target).next().slideToggle();
 })
-$(document).on('click','.image-click-link',function(e){
-  window.location.href = $(e.target).parents('.mobile-show').find('.is--href-replaced').attr('href');
+$(document).on('click','[slider-item]',function(e){
+  
+  if(!$(e.target).hasClass('.slide-dot')){
+    window.location.href = $(e.target).parents('.mobile-show').find('.is--href-replaced').attr('href');
+  }
 })
